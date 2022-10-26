0d6e20e563907ccd75bde6caa654b6fa51f621ce
cbmarie255 <court.b1993@gmail.com>
2022-10-26 11:48:57 -0500

    changing synrax again idk.

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
--- Preparing a MySQL sever for the project with given parameters.
--- Developing database for server.
+-- Preparing a MySQL sever for the project with given parameters
+-- Developing database for server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
