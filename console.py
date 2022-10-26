ead044eb088410910ed811bcbdbf287acb263d40
cbmarie255 <court.b1993@gmail.com>
2022-10-26 10:38:11 -0500

    using .partition instead of split

--- console.py
+++ console.py
@@ -126,8 +126,8 @@ class HBNBCommand(cmd.Cmd):
         new_instance = HBNBCommand.classes[parameters[0]]()
         parameters_dict = {}
         for item in parameters[1:]:
-            attribute = item.split('=')
-            parameters_dict[item[0]] = item[2].replace('"', '\\').replace('_', ' ')
+            attribute = item.partition("=")
+            parameters_dict[item[0]] = item[2].replace('"', '').replace('_', ' ')
         new_instance.__dict__.update(**parameters_dict)
         storage.save()
         print(new_instance.id)
