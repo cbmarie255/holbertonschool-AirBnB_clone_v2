02389bd0197c2d73bc359f3944c9c4fe9713ae09
cbmarie255 <court.b1993@gmail.com>
2022-11-15 13:34:04 -0800

    Adding new template with if statement

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -47,5 +47,12 @@ def route_for_number_template(n=None):
     if type(n) is int:
         return render_template('5-number.html', n=n)
 
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def route_for_number_odd_even(n=None):
+    '''Feeding a string to return as a response.'''
+    if type(n) is int:
+        return render_template('6-number_odd_or_even.html', n=n)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
