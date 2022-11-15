5afcbe3965522f55fdc09614039c806ae2cab8c7
cbmarie255 <court.b1993@gmail.com>
2022-11-15 12:38:52 -0800

    Adding a route to numbers

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    A script that starts a Flask web application with parameters.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''returning a string as a response.'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def route_dos():
+    '''returning a string as a response.'''
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def route_for_c(text=None):
+    '''Feeding a string to return as a response.'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def route_for_python(text='is cool'):
+    '''Feeding a string to return as a response.'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def route_for_ints(n=None):
+    '''Feeding a string to return as a response.'''
+    if type(n) is int:
+        return '{} is a number'.format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
