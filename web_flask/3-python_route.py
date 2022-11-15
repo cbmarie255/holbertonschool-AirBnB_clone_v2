c219d7fd4f336a4b1075aa00590258eae6bc560e
cbmarie255 <court.b1993@gmail.com>
2022-11-15 12:10:42 -0800

    Adding a route for Python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    A script that starts a Flask web application with parameters.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''returning a string as a response.'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def route_dos():
+    '''returning a string as a response.'''
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def route_for_c(text=None):
+    '''Feeding a string to return as a response.'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<string:text>', strict_slashes=False)
+def route_for_python(text='is_cool'):
+    '''Feeding a string to return as a response.'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
